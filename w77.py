3f57fa4ed34f82965a3f2b829a67e5eef279b245
zhamal12 <glnrbdkrmv@gmail.com>
2026-01-24 20:34:14 +0500

    add w7.py

---
+++ w77.py
@@ -0,0 +1,51 @@
+#1 
+x = 1
+y = 2.8 
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2Int
+
+x = 1
+y = 274738548
+z = -43829
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3 Float
+x = 1.10
+y =  1.0
+z = -23.67
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4 Float
+
+x = 35e4 
+y = 12E3
+z = -87.1e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#5 
+x = 3+6j
+y = 5j
+z = -7j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#6
+import random
+
+print(random.randrange(1,10))
