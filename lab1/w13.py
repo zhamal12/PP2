9d60e9b4fc18b7600362e9dbd81cafc0019233c2
zhamal12 <glnrbdkrmv@gmail.com>
2026-01-28 00:42:02 +0500

    Create w13.py

---
+++ lab1/w13.py
@@ -0,0 +1,19 @@
+#1
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
+
+#2
+txt = 'It\'s alright.'
+print(txt) 
+
+#3
+txt = "Hello\nWorld!"
+print(txt) 
+
+#4
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+#5
+txt = "Hello\tWorld!"
+print(txt) 
