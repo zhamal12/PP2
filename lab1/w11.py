9dc6df3af29ee88a2f370d1770f10aaae2cd8b37
zhamal12 <glnrbdkrmv@gmail.com>
2026-01-28 00:33:46 +0500

    Create w11.py

---
+++ lab1/w11.py
@@ -0,0 +1,27 @@
+#1
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#2
+
+name = input()
+print("Hello, " + name + "!")
+
+#3
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#4
+a = input()
+b = input()
+print(a + b)
+
+#5
+a = "2"
+b = "5"
+print(a + b)
