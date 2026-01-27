5b111bcb6822760c1f619f6b383a8d57de2dfb1b
zhamal12 <glnrbdkrmv@gmail.com>
2026-01-28 00:28:27 +0500

    Create w10.py

---
+++ lab1/w10.py
@@ -0,0 +1,19 @@
+#1
+a = "Hello, World!"
+print(a.upper())
+
+#2
+a = "Hello, World!"
+print(a.lower())
+
+#3
+a = " Hello, World! "
+print(a.strip()) 
+
+#4
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#5
+a = "Hello, World!"
+print(a.split(",")) 
