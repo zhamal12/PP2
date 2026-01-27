20ad5ce3089ec84cd3cf0ea1997334782a5a054a
zhamal12 <glnrbdkrmv@gmail.com>
2026-01-28 00:23:27 +0500

    Create w9.py

---
+++ lab1/w9.py
@@ -0,0 +1,19 @@
+#1
+b = "Hello, World!"
+print(b[2:5])
+
+#2
+b = "Hello, World!"
+print(b[:5])
+
+#3
+b = "Hello, World!"
+print(b[2:])
+
+#4
+b = "Hello, World!"
+print(b[-5:-2])
+
+#5
+x = 'Welcome'
+print(x[3:5])
